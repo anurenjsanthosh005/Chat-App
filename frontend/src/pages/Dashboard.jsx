c83b7c58b1494d09053fcd788f6c9977f2be737d
anurenj <anurenjsanthosh005@gmail.com>
2025-05-19 21:56:18 -0700

    added lazy loading to mainChat component in Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import NavBar from "../components/main/NavBar";
 import ChatList from "../components/chat/ChatList";
-import MainChat from "../components/chat/MainChat";
 import MainChatSkeleton from "../components/chat/MainChatSkeleton";
 import { useActiveUsers } from "../contexts/UsersContext";
+import React, { lazy, Suspense } from "react";
+
+const MainChat = lazy(() => import("../components/chat/MainChat"));
 
 const Dashboard = () => {
   const { selectedUser } = useActiveUsers();
@@ -11,12 +13,18 @@ const Dashboard = () => {
   return (
     <Box>
       <NavBar />
-      <Box display="flex" width="100%" height="90vh" sx={{bgcolor:'#f5e8ea'}}>
+      <Box display="flex" width="100%" height="90vh" sx={{ bgcolor: '#f5e8ea' }}>
         <Box width="25%" borderRight="1px solid #ccc" overflow="auto">
           <ChatList />
         </Box>
         <Box width="75%" bgcolor="#cb8c95a6" overflow="hidden">
-          {selectedUser ? <MainChat /> : <MainChatSkeleton />}
+          {selectedUser ? (
+            <Suspense fallback={<MainChatSkeleton />}>
+              <MainChat />
+            </Suspense>
+          ) : (
+            <MainChatSkeleton />
+          )}
         </Box>
       </Box>
     </Box>
